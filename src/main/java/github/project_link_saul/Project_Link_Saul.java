d3ca4badeff4db4264be4f694fcfef1b274fc790
SaulGarciaaaa <saul.garciagiron@iesmiguelherrero.com>
2023-12-22 16:07:08 +0100

    He añadido nuevo texto

--- src/main/java/github/project_link_saul/Project_Link_Saul.java
+++ src/main/java/github/project_link_saul/Project_Link_Saul.java
@@ -13,6 +13,8 @@ public class Project_Link_Saul {
     public static void main(String[] args) {
         System.out.println("Hello Saul! What's up?");
         System.out.println("Holaaaaaaaaaaa");
-        System.out.println("oooooooooooooo");
+        
+        
+        System.out.println("Es viernes!!");
     }
 }
