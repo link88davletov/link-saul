77c97e68a3801e059c13589aaa924e7c8fa59ead
link88davletov <linkkuanysh.davletovdavletov@iesmiguelherrero.com>
2023-12-22 15:37:55 +0100

    añadido nuevo texto

---
+++ src/main/java/github/project_link_saul/Project_Link_Saul.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package github.project_link_saul;
+
+/**
+ *
+ * @author DAM111
+ */
+public class Project_Link_Saul {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Saul! What's up?");
+    }
+}
